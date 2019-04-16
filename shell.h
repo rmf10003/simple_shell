7af209659be8e6f253e26c132c00609d53eb6f05
rmf10003 <685@holbertonschool.com>
2019-04-16 00:02:59 +0000

    add prototypes for 4 fctns, add 2 globals in the globes struct

--- shell.h
+++ shell.h
@@ -8,7 +8,20 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <errno.h>
+ #include <signal.h>
 
+/**
+ * struct GlobalStruct - contains all globals
+ * @argv0: first argument when initiating hsh
+ * @argv0_len: len of first arg
+ * @line: buffer to store command line input
+ * @pathTokes: tokenized path array
+ * @argTokes: tokenized arg array
+ * @last_exit_status: exit status of prev command
+ * @prompt_count: prompt count
+ * @errstr: error string to pass to perror
+ * @error: set to ENOENT standard bur changes if execve executes
+ */
 struct GlobalStruct {
 	char *argv0;
 	int argv0_len;
@@ -17,6 +30,8 @@ struct GlobalStruct {
 	char **argTokes;
 	int last_exit_status;
 	int prompt_count;
+	char *errstr;
+	int error;
 } globes;
 
 typedef struct builtin {
@@ -30,8 +45,9 @@ char **tokenizeEnvVar(char *var);
 void isInteractiveMode(void);
 void handleCommand(char **argv, char **argTokes, char **pathTokes);
 char **createTokes(char *str, ssize_t size, char *delim);
-void parser(char **argTokes, char **pathTokes);
+int parser(char **argTokes, char **pathTokes);
 char *_getenv(const char *name);
+void signal_handler(int sig);
 
 /* builtins and caller fctn */
 int (*checkBuiltin(char *cmd))(void);
@@ -44,9 +60,14 @@ size_t _strlen(char *str);
 char *_strcpy(char *dest, const char *src);
 char *str_concat(char *s1, char *s2);
 void freeTokes(char **tokes);
-void set_error(void);
 char *_itoa(int num);
 void _rev(char *buff_boi_cpy);
+char *_strcat(char *dest, char *src);
 
+
+/* error handeling */
+char *create_perror_string(char *prompt_count, int pc_len);
+void print_error(char *errstr, char *prompt_count, int pc_len);
+void _error(void);
 
 #endif
