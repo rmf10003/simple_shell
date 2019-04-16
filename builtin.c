e5e3480f7261ee3409be7f3f1e0f82c53632a204
HausCloud <696@holbertonschool.com>
2019-04-16 03:11:31 +0000

    add _atoi and _pow

--- builtin.c
+++ builtin.c
@@ -49,7 +49,7 @@ int envB(void)
 
 /**
  * exitB - free allocated stuff and then exit
- * Return: 1 on success
+ * Return: 2 on failure
  */
 
 int exitB(void)
@@ -58,21 +58,74 @@ int exitB(void)
 
 	if (globes.argTokes[1] != NULL)
 	{
-		if (globes.argTokes[1][0] < '0' globes.argTokes[1][0] > '9')
+		if (globes.argTokes[1][0] < '0' && globes.argTokes[1][0] > '9')
 			if (globes.argTokes[1][0] != '+')
+			{
+				_error();
 				return (2);
+			}
 		for (; globes.argTokes[1][i]; i++)
-			if (globes.argTokes[1][i] < '0' globes.argTokes[1][i] > '9')
+			if (globes.argTokes[1][i] < '0' && globes.argTokes[1][i] > '9')
+			{
+				_error();
 				return (2);
+			}
 		if (_strlen(globes.argTokes[1]) > 11)
+		{
+			_error();
 			return (2);
+		}
 		if (globes.argTokes[1][0] == '+')
 			manual_exit = _atoi(*(globes.argTokes + 1) + 1);
 		else
 			manual_exit = _atoi(*globes.argTokes);
+		if (manual_exit > INT_MAX)
+		{
+			_error();
+			return (2);
+		}
+		free(globes.line);
+		freeTokes(globes.argTokes);
+		freeTokes(globes.pathTokes);
+		exit(manual_exit & 0377);
 	}
 	free(globes.line);
 	freeTokes(globes.argTokes);
 	freeTokes(globes.pathTokes);
 	exit(globes.last_exit_status);
+}
+
+/**
+ * _atoi - convert a char array into an int
+ * @str: char string to convert
+ *
+ * Return: integer rep of char str
+ */
+int _atoi(char *str)
+{
+	int exit_status = 0;
+	int i = -1;
+	char *cpy = str;
+
+	for (; cpy[i]; i++)
+		;
+	for (; *cpy; cpy++, i--)
+		exit_status += (*str - '0') * _pow(10, i);
+	return (exit_status);
+}
+/**
+ * _pow - returns val of x raised to y
+ * @x: base int
+ * @y: power int
+ *
+ * Return: val of x raised to y
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow(x, y - 1));
 }
