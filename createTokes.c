115cf688c0acf198e85d9d2703717ec53970519d
rmf10003 <685@holbertonschool.com>
2019-04-17 18:22:40 +0000

    implement # handeling

--- createTokes.c
+++ createTokes.c
@@ -24,17 +24,19 @@ char **createTokes(char *str, ssize_t size, char *delim)
 		free(cpy);
 		return (NULL);
 	}
-	_strcpy(cpy, str);
-	_strcpy(cpy2, str);
+	_strcpy(cpy, str), _strcpy(cpy2, str);
 	token = strtok(cpy2, delim);
-	if (token == NULL)
+	for (numPtrs = 2; token; numPtrs++)
 	{
-		free(cpy);
-		free(cpy2);
+		if (*token == '#')
+			break;
+		token = strtok(NULL, delim);
+	}
+	if (numPtrs == 2)
+	{
+		free(cpy), free(cpy2);
 		return (NULL);
 	}
-	for (numPtrs = 2; token; numPtrs++)
-		token = strtok(NULL, delim);
 	free(cpy2);
 	tokenArr = malloc(sizeof(char *) * numPtrs);
 	if (!tokenArr)
@@ -42,9 +44,8 @@ char **createTokes(char *str, ssize_t size, char *delim)
 		free(cpy);
 		return (NULL);
 	}
-	*tokenArr++ = cpy;
-	token = strtok(cpy, delim);
-	for (i = 0; token != NULL; i++)
+	*tokenArr++ = cpy, token = strtok(cpy, delim);
+	for (i = 0; i < (numPtrs - 2); i++)
 	{
 		tokenArr[i] = token;
 		token = strtok(NULL, delim);
